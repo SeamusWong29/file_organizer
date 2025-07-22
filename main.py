0a3bd525355182515dfcf000eafc69ab1a1994be
Seamus <S4101434@student.rmit.edu.au>
2025-07-22 10:05:20 +1000

    refactor: extract logging to a separate function

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ print("Hello, file sorting world!")
 def scan_folder():
     print("Scanning folder...")
 
+def log(message):
+    with open("log.txt", "a") as f:
+        f.write(message + "\n")
+
 scan_folder()
